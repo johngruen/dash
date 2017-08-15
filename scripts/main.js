bf94369e7700be7fd4d847e2daae6cc2853c2e37
John Gruen <john.gruen@gmail.com>
2017-08-15 13:53:23 +0200

    add clocks

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,30 @@
 $(() => {
   $('button').eq(0).focus();
-  $('button').click(function () {
+  $('button').click(function() {
     window.open($(this).attr('data-href'), '_newtab');
   });
+
+  clock();
+  setTimeout(clock, 60 * 1000);
 });
+
+const clock = () => {
+  $('.clock').each(function() {
+    const locale = $(this).attr('data-tz');
+    const time = moment.tz(locale);
+    const m = time.format('mm');
+    const h = time.format('hh');
+    const a = time.format('a');
+
+    if((a === 'am' && h >= 9) || (a === 'pm' && h <= 6)) {
+      $(this).addClass('working');
+    } else if ($(this).hasClass('working')) {
+      $(this).removeClass('working');
+    }
+
+    $(this).find('.minute').css('transform',
+      `rotate(${(m/60) * 360}deg)`);
+    $(this).find('.hour').css('transform', 
+      `rotate(${(h/12 + m/(60 * 12)) * 360}deg)`);
+  });
+}
