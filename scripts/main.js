5a13ded9dd9dc6c1e70d0dc9546ce0d72240dd31
John Gruen <john.gruen@gmail.com>
2017-08-17 14:52:23 +0200

    add simple pom

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,25 @@ $(() => {
     window.open($(this).attr('data-href'), '_newtab');
   });
 
+  $('.pomodoro').click(function(){
+    if( $(this).hasClass('active')) return;
+    Notification.requestPermission().then(function(result) {
+      $('.pomodoro').css('border-color', '#9d2cac')
+                    .addClass('active').find('h2')
+                    .text('WORK IT!');
+      setTimeout(function() {
+        const notification = new Notification("Take 5!");
+        $('.pomodoro').find('h2').text('CHILLIN');
+        setTimeout(function(){
+          const notification = new Notification("Done!");
+          $('.pomodoro').css('border-color', '#04c4c7')
+              .removeClass('active').find('h2')
+              .text('pomodoro');
+        }, 1000 * 60 * 5);
+      }, 1000 * 60 * 25);
+    });
+  });
+
   clock();
   setTimeout(clock, 60 * 1000);
 });
