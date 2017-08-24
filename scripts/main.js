12947226ac625df44fb6ebb2ea8a5bc7798efa08
John Gruen <john.gruen@gmail.com>
2017-08-24 12:59:16 -0400

    better pom

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,107 @@
-$(() => {
-  $('button').eq(0).focus();
+$(document).ready(function() {
+
+  Notification.requestPermission();
+
+  $('.pom--form').submit(function(e) {
+    e.preventDefault();
+    if(Pomodoro.isValid()) {
+      Pomodoro.setTimings();
+      Pomodoro.flipState(Pomodoro.activeClass, '.pom--work');
+      Pomodoro.run();
+      const notification = new Notification('Pomodoro started');
+    }
+  });
+
+  $('.pom--work').click(function(){
+    Pomodoro.flipState(Pomodoro.activeClass, '.pom--set');
+    Pomodoro.state = 2;
+  });
+
   $('button').click(function() {
     window.open($(this).attr('data-href'), '_blank');
   });
 
-  $('.pomodoro').click(function(){
-    if( $(this).hasClass('active')) return;
-    Notification.requestPermission().then(function(result) {
-      $('.pomodoro').css('border-color', '#9d2cac')
-                    .addClass('active').find('h2')
-                    .text('WORK IT!');
-      setTimeout(function() {
-        const notification = new Notification("Take 5!");
-        $('.pomodoro').find('h2').text('CHILLIN');
-        setTimeout(function(){
-          const notification = new Notification("Done!");
-          $('.pomodoro').css('border-color', '#04c4c7')
-              .removeClass('active').find('h2')
-              .text('pomodoro');
-        }, 1000 * 60 * 5);
-      }, 1000 * 60 * 25);
-    });
-  });
-
+  Pomodoro.init();
+  $('button:eq(0)').focus();
   clock();
   setInterval(clock, 60 * 1000);
 });
+
+const Pomodoro = {
+  activeClass: '.pom--set',
+  state: 0,
+  timings: [],
+  timer: [],
+  init() {
+    const self = this;
+    $('.pom--set input[type=number]').each(function(i) {
+      const multiplier = (i === 2) ? 1 : 60;
+      self.timings.push($(this).val() * multiplier);
+      self.timer.push($(this).val() * multiplier);
+    });
+  },
+
+  isValid() {
+    let isTrue = true;
+    $('.pom--set input[type=number]').each(function() {
+      const value = $(this).val();
+      if( value <= 0 || value >= 999 ) isTrue = false;
+    });
+    return isTrue;
+  }, 
+
+  setTimings() {
+    const self = this;
+    $('.pom--set input[type=number]').each(function(i) {
+      const multiplier = (i === 2)?  1 : 60;
+      self.timings[i] = $(this).val() * multiplier;
+      self.timer[i] = $(this).val() * multiplier;
+    });
+  },
+
+  flipState(start, end) {
+    $(start).hide();
+    $(end).css('display','flex');
+    this.activeClass = end;
+  },
+
+  draw() {
+    const width = 100 * (this.timer[this.state]/this.timings[this.state]) + '%';
+    const label = (this.state) ? 'Chill' : 'Work';
+    $('.remaining').css('width', width);
+    $('.pom--work h2').text(label);
+  },
+
+  run() {
+    this.draw();
+    setTimeout(() => {
+      this.timer[this.state]--;
+      if (this.state <= 1) {
+        if(this.timer[this.state] >= 0) {
+          if (this.timer[this.state] === 0) {
+            this.state++;
+            if (this.state === 1) {
+              const notification = new Notification('Chill Out');
+            }
+          }
+        }
+        this.run();
+      } else {
+        if(this.timer[2] > 0) {
+          this.state = 0;
+          this.timer[0] = this.timings[0];
+          this.timer[1] = this.timings[1];
+          const notification = new Notification('Back to work');
+          this.run();
+        } else {
+          this.state = 0;
+          this.flipState(this.activeClass, '.pom--set');
+          const notification = new Notification('Done');
+        }
+      }
+    }, 1000);
+  }
+};
 
 const clock = () => {
   $('.clock').each(function() {
